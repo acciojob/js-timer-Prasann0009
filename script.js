6515e8c83b9882b4aa16d5131f301eba25a66cff
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-09-30 22:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// write js code here if required
+const timerElement = document.getElementById("timer");
+
+function formatTwoDigits(number) {
+    return number < 10 ? `0${number}` : `${number}`;
+}
+
+function updateTimer() {
+    const now = new Date();
+    const day = formatTwoDigits(now.getDate());
+    const month = formatTwoDigits(now.getMonth() + 1); // Months are zero-based
+    const year = now.getFullYear();
+    const hours = formatTwoDigits(now.getHours());
+    const minutes = formatTwoDigits(now.getMinutes());
+    const seconds = formatTwoDigits(now.getSeconds());
+
+    timerElement.textContent = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTimer, 1000);
+updateTimer();
